04a90f0a7a4ba79879456e3da7fba05623bd5ffb
marcellszalai <szalai.marcell136@gmail.com>
2024-12-02 19:03:30 +0100

    fix paths

--- backend/quantum_sanctum/config/settings.py
+++ backend/quantum_sanctum/config/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'quantum_sanctum.urls'
+ROOT_URLCONF = 'quantum_sanctum.routes.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'quantum_sanctum.wsgi.application'
+WSGI_APPLICATION = 'quantum_sanctum.config.wsgi.application'
 
 
 # Database
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'quantum_sanctum.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'databases/db.sqlite3',
+        'NAME': BASE_DIR / 'databases' / 'db.sqlite3',
     }
 }
 
